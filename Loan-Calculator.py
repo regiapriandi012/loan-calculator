77a66ca3e063cb75315b9117e6744c71860f62eb
regiapriandi012 <regiapriandi024@gmail.com>
2022-06-22 11:20:24 +0700

    Stage 1

---
+++ Loan-Calculator.py
@@ -0,0 +1,12 @@
+loan_principal = 'Loan principal: 1000'
+final_output = 'The loan has been repaid!'
+first_month = 'Month 1: repaid 250'
+second_month = 'Month 2: repaid 250'
+third_month = 'Month 3: repaid 500'
+
+# write your code here
+print(loan_principal)
+print(first_month)
+print(second_month)
+print(third_month)
+print(final_output)
